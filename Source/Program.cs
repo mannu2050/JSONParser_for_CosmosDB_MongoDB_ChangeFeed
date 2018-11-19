becd7172b08950d77c7dd6aade9b1e9b44b35ab2
Manish Sharma <3263658+mannu2050@users.noreply.github.com>
2018-11-19 23:23:05 +0530

    Added source

---
+++ Source/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader f = new System.IO.StreamReader("json1.json");
+            string jsonContent = f.ReadToEnd();
+            int i = jsonContent.IndexOf("stockName");
+            int j = jsonContent.Substring(i).IndexOf("$v");
+
+            string stockSymbol = jsonContent.Substring( i+j+ 6,4);
+            jsonContent = jsonContent.Substring(i + j + 10);
+            i = jsonContent.IndexOf("stockValue");
+            j = jsonContent.Substring(i).IndexOf("$v");
+            
+            string stockValue = jsonContent.Substring(j + i + 4, jsonContent.Substring(j + i).IndexOf("\n") - 4).Trim();
+
+            jsonContent = jsonContent.Substring(i + j + 10);
+            i = jsonContent.IndexOf("stockValueAt");
+            j = jsonContent.Substring(i).IndexOf("$v");
+
+            string stockValueAt = jsonContent.Substring(j + i + 4, jsonContent.Substring(j + i).IndexOf("\n") - 4).Trim();
+        }
+    }
+}
+
+           
